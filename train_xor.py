4cb394cb3f606941f79391dfb8fd841e49b3bd5d
Alex Nichol <unixpickle@gmail.com>
2018-12-09 10:20:52 -0500

    add xor example

---
+++ train_xor.py
@@ -0,0 +1,21 @@
+"""
+A demonstration on the xor problem.
+
+This technique only works on xor because of randomness.
+"""
+
+import numpy as np
+
+from output_boosting import learn
+
+
+def main():
+    learn(samples=np.array([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float32),
+          labels=np.array([[1, 0], [0, 1], [0, 1], [1, 0]], dtype=np.float32),
+          depth=1000,
+          lr=1e-4,
+          log_fn=print)
+
+
+if __name__ == '__main__':
+    main()
